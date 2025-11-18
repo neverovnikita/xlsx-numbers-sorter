6c5b5fbdb10a55946d6caafb7e456b2e59855286
neverovnikita <153305920+neverovnikita@users.noreply.github.com>
2025-11-18 23:46:02 +0700

    initial commit

---
+++ src/test/java/com/neverov/xlsxnumberssorter/XlsxNumbersSorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neverov.xlsxnumberssorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsxNumbersSorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
